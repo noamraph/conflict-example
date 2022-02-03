6738c47e144bad93be16893a484f92d8dd955640
Noam Yorav-Raphael <noamraph@gmail.com>
2022-02-03 18:10:58 +0200

    Initial commit

---
+++ calc.py
@@ -0,0 +1,7 @@
+def subtract(name, a, b):
+    print(f"Hi {name}, let's subtract!")
+    print(f'{a} - {b} = {a - b}')
+
+def add(name, a, b):
+    print(f"Hi {name}, let's add!")
+    print(f'{a} + {b} = {a + b}')
