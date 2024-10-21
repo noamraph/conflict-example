85e0568ad58cf150586c137df6c1477a661e4f30
Noam Yorav-Raphael <noamraph@gmail.com>
2024-10-21 10:33:22 +0300

    Add calc.py, with functions for dividing and multiplying

---
+++ calc.py
@@ -0,0 +1,7 @@
+def divide(name, a, b):
+    print(f"Hi {name}, let's divide!")
+    print(f'{a} / {b} = {a / b}')
+
+def multiply(name, a, b):
+    print(f"Hi {name}, let's multiply!")
+    print(f'{a} * {b} = {a * b}')
