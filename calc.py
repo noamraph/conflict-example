2a8ccf6b13499c8b30ff8bfc5a1a1d8175bb46bc
Noam Yorav-Raphael <noamraph@gmail.com>
2024-10-21 10:34:35 +0300

    Multiply 3 numbers instead of 2

--- calc.py
+++ calc.py
@@ -2,6 +2,6 @@ def divide(name, a, b):
     print(f"Hi {name}, let's divide!")
     print(f'{a} / {b} = {a / b}')
 
-def multiply(name, a, b):
+def multiply(name, a, b, c):
     print(f"Hi {name}, let's multiply!")
-    print(f'{a} * {b} = {a * b}')
+    print(f'{a} * {b} * {c} = {a * b * c}')
