67e83b0cc5566fe24efd685841c75ed486ba4df4
Noam Yorav-Raphael <noamraph@gmail.com>
2026-02-23 23:57:25 +0200

    test.py: Add --smoke-runs and --exec-sequence and simplify stuff

--- test.py
+++ test.py
@@ -1,19 +1,17 @@
 def build_test_args(args: TestOptions) -> list[str]:
-    if args.run_id is not None:
-        run_id, run_name = args.run_id, args.run_name
-    else:
-        run_name = f"TEST-{'ALT' if args.special else 'STD'}-{args.platform}-{args.logger}-{args.exec_mode}"
-        run_id = os.urandom(8).hex()
-    dry_run_args = ["--dry-run"] if args.dry_run else []
-    dry_fails_args = ["--dry-fails", args.dry_fails] if args.dry_fails else []
     return (
-        dry_run_args
-        + dry_fails_args
+        (["--dry-run"] if args.dry_run else [])
+        + (["--dry-fails", args.dry_fails] if args.dry_fails else [])
+        + (["--tests", args.tests] if args.tests else [])
         + [
+            "--smoke-runs",
+            str(args.smoke_runs),
+            "--exec-sequence",
+            args.exec_sequence,
             "--run-id",
-            run_id,
+            args.run_id,
             "--run-name",
-            run_name,
+            args.run_name,
             "--engine",
             args.test_engine,
             args.results_dir,
