0e0751c46bd15f985d6952913033dc90096d42d1
Noam Yorav-Raphael <noamraph@gmail.com>
2026-02-23 23:52:07 +0200

    Create test.py

---
+++ test.py
@@ -0,0 +1,26 @@
+def build_test_args(args: TestOptions) -> list[str]:
+    if args.run_id is not None:
+        run_id, run_name = args.run_id, args.run_name
+    else:
+        run_name = f"TEST-{'ALT' if args.special else 'STD'}-{args.platform}-{args.logger}-{args.exec_mode}"
+        run_id = os.urandom(8).hex()
+    dry_run_args = ["--dry-run"] if args.dry_run else []
+    dry_fails_args = ["--dry-fails", args.dry_fails] if args.dry_fails else []
+    return (
+        dry_run_args
+        + dry_fails_args
+        + [
+            "--run-id",
+            run_id,
+            "--run-name",
+            run_name,
+            "--engine",
+            args.test_engine,
+            args.results_dir,
+            args.config_bundle,
+            args.platform,
+            args.logger,
+            args.exec_mode,
+        ]
+    )
+
